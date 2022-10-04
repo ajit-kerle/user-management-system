291c5d1ebaed748de23b067682c4021ceedd822f
ajit kerle <ajitkerle70@gmail.com>
2022-10-04 10:36:39 +0530

    ejs apply to all

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
+app.get('/add-user',(req,res)=>{
+    res.render('add_user')
+})
+
+app.get('/update-user',(req,res)=>{
+    res.render('update')
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
 })
